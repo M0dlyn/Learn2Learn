77fb968002d66959567d975fa073be626103ba35
xvvcs <maciek.m2004@wp.pl>
2025-05-02 18:54:36 +0200

    added and run database migrations, sqlite setup

---
+++ Learn2Learn/app/Models/Note.php
@@ -0,0 +1,49 @@
+<?php
+
+namespace App\Models;
+
+use Illuminate\Database\Eloquent\Factories\HasFactory;
+use Illuminate\Database\Eloquent\Model;
+use Illuminate\Database\Eloquent\Relations\BelongsTo;
+use Illuminate\Database\Eloquent\Relations\BelongsToMany;
+
+class Note extends Model
+{
+    use HasFactory;
+
+    /**
+     * The attributes that are mass assignable.
+     *
+     * @var array<int, string>
+     */
+    protected $fillable = [
+        'title',
+        'content',
+        'user_id',
+        'learning_technic_id',
+    ];
+
+    /**
+     * Get the student that owns the note.
+     */
+    public function student(): BelongsTo
+    {
+        return $this->belongsTo(Student::class, 'user_id');
+    }
+
+    /**
+     * Get the learning technic that the note uses.
+     */
+    public function learningTechnic(): BelongsTo
+    {
+        return $this->belongsTo(LearningTechnic::class);
+    }
+
+    /**
+     * Get the tags for the note.
+     */
+    public function tags(): BelongsToMany
+    {
+        return $this->belongsToMany(Tag::class, 'tag_notes');
+    }
+}
---
+++ Learn2Learn/app/Models/Tag.php
@@ -0,0 +1,29 @@
+<?php
+
+namespace App\Models;
+
+use Illuminate\Database\Eloquent\Factories\HasFactory;
+use Illuminate\Database\Eloquent\Model;
+use Illuminate\Database\Eloquent\Relations\BelongsToMany;
+
+class Tag extends Model
+{
+    use HasFactory;
+
+    /**
+     * The attributes that are mass assignable.
+     *
+     * @var array<int, string>
+     */
+    protected $fillable = [
+        'name',
+    ];
+
+    /**
+     * Get the notes for the tag.
+     */
+    public function notes(): BelongsToMany
+    {
+        return $this->belongsToMany(Note::class, 'tag_notes');
+    }
+}
